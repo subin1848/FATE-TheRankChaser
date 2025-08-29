3ee63f5d5f93d26fb09c2b5c0924c3eb9b77a166
subin1848 <s2315@e-mirim.hs.kr>
2025-08-29 16:12:29 +0900

    #1 사용자 화면 설계 완성

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -35,16 +35,19 @@
             this.정보ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.rankChaserToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.label1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.tbpoint = new System.Windows.Forms.TextBox();
             this.label5 = new System.Windows.Forms.Label();
             this.cbStrength = new System.Windows.Forms.ComboBox();
-            this.comboBox3 = new System.Windows.Forms.ComboBox();
-            this.cbweakness = new System.Windows.Forms.ComboBox();
+            this.cbGender = new System.Windows.Forms.ComboBox();
+            this.cbWeakness = new System.Windows.Forms.ComboBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.dtBirth = new System.Windows.Forms.DateTimePicker();
+            this.label7 = new System.Windows.Forms.Label();
+            this.tbUser = new System.Windows.Forms.TextBox();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,7 +58,7 @@
             this.정보ToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(381, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(487, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -73,12 +76,14 @@
             this.내역불러오기ToolStripMenuItem.Name = "내역불러오기ToolStripMenuItem";
             this.내역불러오기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.내역불러오기ToolStripMenuItem.Text = "내역 불러오기";
+            this.내역불러오기ToolStripMenuItem.Click += new System.EventHandler(this.내역불러오기ToolStripMenuItem_Click_1);
             // 
             // 긑ToolStripMenuItem
             // 
             this.긑ToolStripMenuItem.Name = "긑ToolStripMenuItem";
             this.긑ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.긑ToolStripMenuItem.Text = "끝내기";
+            this.긑ToolStripMenuItem.Click += new System.EventHandler(this.긑ToolStripMenuItem_Click);
             // 
             // 정보ToolStripMenuItem
             // 
@@ -97,23 +102,16 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(13, 48);
+            this.label1.Location = new System.Drawing.Point(70, 150);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(216, 12);
+            this.label1.Size = new System.Drawing.Size(129, 12);
             this.label1.TabIndex = 1;
-            this.label1.Text = "생년월일을 입력하세요 ex) 2007-03-02";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(15, 63);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
-            this.textBox1.TabIndex = 2;
+            this.label1.Text = "생년월일을 입력하세요";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(13, 103);
+            this.label2.Location = new System.Drawing.Point(70, 205);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(105, 12);
             this.label2.TabIndex = 3;
@@ -122,7 +120,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(13, 182);
+            this.label3.Location = new System.Drawing.Point(66, 284);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(185, 12);
             this.label3.TabIndex = 5;
@@ -132,23 +130,23 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(13, 238);
+            this.label4.Location = new System.Drawing.Point(67, 340);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(185, 12);
             this.label4.TabIndex = 7;
             this.label4.Text = "당신의 약한 포지션을 골라주세요";
             // 
-            // textBox5
+            // tbpoint
             // 
-            this.textBox5.Location = new System.Drawing.Point(15, 305);
-            this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(151, 21);
-            this.textBox5.TabIndex = 10;
+            this.tbpoint.Location = new System.Drawing.Point(69, 407);
+            this.tbpoint.Name = "tbpoint";
+            this.tbpoint.Size = new System.Drawing.Size(182, 21);
+            this.tbpoint.TabIndex = 10;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(13, 290);
+            this.label5.Location = new System.Drawing.Point(67, 392);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(219, 12);
             this.label5.TabIndex = 9;
@@ -164,61 +162,98 @@
             "올라운더",
             "비주얼",
             "예능"});
-            this.cbStrength.Location = new System.Drawing.Point(15, 197);
+            this.cbStrength.Location = new System.Drawing.Point(69, 299);
             this.cbStrength.Name = "cbStrength";
             this.cbStrength.Size = new System.Drawing.Size(183, 20);
             this.cbStrength.TabIndex = 12;
             // 
-            // comboBox3
+            // cbGender
             // 
-            this.comboBox3.FormattingEnabled = true;
-            this.comboBox3.Items.AddRange(new object[] {
+            this.cbGender.FormattingEnabled = true;
+            this.cbGender.Items.AddRange(new object[] {
             "여자",
             "남자"});
-            this.comboBox3.Location = new System.Drawing.Point(15, 118);
-            this.comboBox3.Name = "comboBox3";
-            this.comboBox3.Size = new System.Drawing.Size(121, 20);
-            this.comboBox3.TabIndex = 13;
+            this.cbGender.Location = new System.Drawing.Point(71, 220);
+            this.cbGender.Name = "cbGender";
+            this.cbGender.Size = new System.Drawing.Size(180, 20);
+            this.cbGender.TabIndex = 13;
             // 
-            // cbweakness
+            // cbWeakness
             // 
-            this.cbweakness.FormattingEnabled = true;
-            this.cbweakness.Items.AddRange(new object[] {
+            this.cbWeakness.FormattingEnabled = true;
+            this.cbWeakness.Items.AddRange(new object[] {
             "보컬",
             "댄스",
             "랩",
             "올라운더",
             "비주얼",
             "예능"});
-            this.cbweakness.Location = new System.Drawing.Point(15, 253);
-            this.cbweakness.Name = "cbweakness";
-            this.cbweakness.Size = new System.Drawing.Size(183, 20);
-            this.cbweakness.TabIndex = 14;
+            this.cbWeakness.Location = new System.Drawing.Point(69, 355);
+            this.cbWeakness.Name = "cbWeakness";
+            this.cbWeakness.Size = new System.Drawing.Size(183, 20);
+            this.cbWeakness.TabIndex = 14;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(15, 372);
+            this.button1.Location = new System.Drawing.Point(65, 452);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(354, 53);
             this.button1.TabIndex = 15;
             this.button1.Text = "결과 보기";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 40F);
+            this.label6.Location = new System.Drawing.Point(92, 57);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(311, 54);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "운명의 시작";
+            // 
+            // dtBirth
+            // 
+            this.dtBirth.Location = new System.Drawing.Point(72, 165);
+            this.dtBirth.Name = "dtBirth";
+            this.dtBirth.Size = new System.Drawing.Size(179, 21);
+            this.dtBirth.TabIndex = 17;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(290, 150);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(105, 12);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "이름을 입력하세요";
+            // 
+            // tbUser
+            // 
+            this.tbUser.Location = new System.Drawing.Point(293, 165);
+            this.tbUser.Name = "tbUser";
+            this.tbUser.Size = new System.Drawing.Size(126, 21);
+            this.tbUser.TabIndex = 19;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.ClientSize = new System.Drawing.Size(487, 536);
+            this.Controls.Add(this.tbUser);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.dtBirth);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.cbweakness);
-            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.cbWeakness);
+            this.Controls.Add(this.cbGender);
             this.Controls.Add(this.cbStrength);
-            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.tbpoint);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
@@ -241,16 +276,19 @@
         private System.Windows.Forms.ToolStripMenuItem 정보ToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem rankChaserToolStripMenuItem;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox tbpoint;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.ComboBox cbStrength;
-        private System.Windows.Forms.ComboBox comboBox3;
-        private System.Windows.Forms.ComboBox cbweakness;
+        private System.Windows.Forms.ComboBox cbGender;
+        private System.Windows.Forms.ComboBox cbWeakness;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.DateTimePicker dtBirth;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbUser;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace RankChaser
 {
@@ -26,5 +28,69 @@ namespace RankChaser
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UserData trainee = new UserData
+            {
+                Name = tbUser.Text,   
+                BirthDate = dtBirth.Value,
+                Gender = cbGender.SelectedItem.ToString(),
+                StrongPosition = cbStrength.SelectedItem.ToString(),
+                WeakPosition = cbWeakness.SelectedItem.ToString(),
+                CharmPoint = tbpoint.Text,
+                SimulationDate = DateTime.Now
+            };
+
+            // UserData 객체를 그대로 전달
+            SaveHistory(trainee);
+
+        }
+
+        // 내역 저장 메서드
+        private void SaveHistory(UserData trainee)
+        {
+            try
+            {
+                string filename = "history.csv";
+             
+                // CSV로 한 줄 만들기 (각 속성을 |로 구분)
+                string line = $"{trainee.Name}|{trainee.BirthDate:yyyy-MM-dd}|{trainee.Gender}|{trainee.StrongPosition}|{trainee.WeakPosition}|{trainee.CharmPoint}|{trainee.SimulationDate:yyyy-MM-dd HH:mm:ss}";
+
+                File.AppendAllText(filename, line + Environment.NewLine);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                MessageBox.Show($"권한이 없습니다. \n{ex.Message}", "권한 오류!");
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show($"알 수 없는 오류가 발생했습니다. \n{ex.Message}", "알 수 없는 오류!");
+            }
+        }
+
+        // 사용자 데이터 클래스
+        public class UserData
+        {
+            public string Name { get; set; }
+            public DateTime BirthDate { get; set; }
+            public string Gender { get; set; }
+            public string StrongPosition { get; set; }
+            public string WeakPosition { get; set; }
+            public string CharmPoint { get; set; }
+            public DateTime SimulationDate { get; set; }
+            public int Age => DateTime.Now.Year - BirthDate.Year;
+        }
+
+        // 전략 데이터
+        public class StrategyData
+        {
+            public string Score { get; set; }
+            public string Character { get; set; }
+            public string Strategies { get; set; }
+            public string Icons { get; set; }
+        }
+
     }
 }
